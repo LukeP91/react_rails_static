64c4877220a5116fb2a28dc215e1c00bbf214c78
Luke Pawlik <lukepawlik@gmail.com>
2016-10-09 21:39:49 +0200

    finished task.
    * add first react component and rendered it
    * for now it's just static data

---
+++ app/assets/javascripts/components/example.js.jsx
@@ -0,0 +1,12 @@
+$(() => {
+  ReactDOM.render(
+    <div>
+      <h3>Members of Junior Rails Developer Course:</h3>
+      <ul>
+        <li>Luke Pawlik</li>
+        <li>Yanko</li>
+      </ul>
+    </div>,
+    document.getElementById('start')
+  );
+});
